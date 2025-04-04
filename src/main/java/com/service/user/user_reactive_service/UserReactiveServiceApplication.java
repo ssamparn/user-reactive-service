474d4452ae78dcdeb42d99cc4179eceb0d8dd3f8
Sashank <sashanka.samantray@rabobank.nl>
2025-04-05 00:08:43 +0200

    first commit

---
+++ src/main/java/com/service/user/user_reactive_service/UserReactiveServiceApplication.java
@@ -0,0 +1,13 @@
+package com.service.user.user_reactive_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserReactiveServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserReactiveServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/service/user/user_reactive_service/UserReactiveServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.user.user_reactive_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserReactiveServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
